dc5adbf7bfc915e87cdc5d7d384379ee8918a973
8502 <8502@ait-sydney.net.au>
2021-06-18 11:10:53 +1000

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//comand has to go between tags
+/*multiline
+coments
+*/
+$name="Juan";
+$day='friday';
+$test="Hello PHP";
+
+echo "hello, " .$name. "Today is ". $day;
+echo'<p>',$day .'<p>';
+?>
+
+<div style="background-color:green;color:white;
+">
+<?php echo $test;?>
+</div>
